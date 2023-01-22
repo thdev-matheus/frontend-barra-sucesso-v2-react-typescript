d7c761fa915dc182c983c2a0c2d19d3db8730182
Theus <matheusth.dev@gmail.com>
2023-01-22 01:19:50 -0300

    feat: theme ligh and dark

---
+++ src/styles/Themes.ts
@@ -0,0 +1,51 @@
+export const LightTheme = {
+  primary: "#1abc9c",
+  secondary: "#3498db",
+  tertiary: "#f1c40f",
+
+  error: "#ff4757",
+  success: "#2ed573",
+  warning: "#ffa502",
+
+  black: "#2c3e50",
+  gray: "#7f8c8d",
+  white: "#ecf0f1",
+
+  transparentBlack: "#2c3e5085",
+  transparentGray: "#7f8c8d85",
+  transparentWhite: "#ecf0f185",
+
+  textPrimary: "#2c3e50",
+  textSecondary: "#222f3e",
+  textTertiary: "#303952",
+
+  backgroundPrimary: "#ecf0f1",
+  backgroundSecondary: "#dfe4ea",
+  backgroundTertiary: "#f7f1e3",
+};
+
+export const DarkTheme = {
+  primary: "#1abc9c",
+  secondary: "#3498db",
+  tertiary: "#f1c40f",
+
+  error: "#ff4757",
+  success: "#2ed573",
+  warning: "#ffa502",
+
+  black: "#2c3e50",
+  gray: "#7f8c8d",
+  white: "#ecf0f1",
+
+  transparentBlack: "#2d343685",
+  transparentGray: "#7f8c8d85",
+  transparentWhite: "#ecf0f185",
+
+  textPrimary: "#ecf0f1",
+  textSecondary: "#dfe4ea",
+  textTertiary: "#f7f1e3",
+
+  backgroundPrimary: "#2c3e50",
+  backgroundSecondary: "#222f3e",
+  backgroundTertiary: "#303952",
+};
