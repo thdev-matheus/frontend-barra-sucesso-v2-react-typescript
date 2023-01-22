34a1c5b0cf362cdd82ebb7355a073d877afa94c9
Theus <matheusth.dev@gmail.com>
2023-01-22 01:05:56 -0300

    feat: global styles and themes

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+
+    border: none;
+    outline: none;
+
+    box-sizing: border-box;
+    box-shadow: 0 0 0 0;
+}
+
+  body {
+    width: 100vw;
+    height: 100vh;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+
+    overflow: hidden auto;
+    
+    font-weight: normal;
+
+ }
+`;
