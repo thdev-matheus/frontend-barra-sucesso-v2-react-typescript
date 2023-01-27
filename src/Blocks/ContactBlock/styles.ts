705fc2ba615243595f6983409fd74b73447921f3
Theus <matheusth.dev@gmail.com>
2023-01-27 20:38:57 -0300

    feat: bloco de contato 1900 1440

--- src/Blocks/ContactBlock/styles.ts
+++ src/Blocks/ContactBlock/styles.ts
@@ -27,24 +27,7 @@ export const Container = styled.section<T.IContainerProps>`
   }
 
   @media (min-width: 1440px) {
-    width: 100vw;
     min-height: 15rem;
-
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-
-    scroll-margin-top: 5rem;
-
-    background-color: ${(props) => props.theme.backgroundSecondary};
-    background-image: url(${(props) => props.bgImg});
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-  }
-
-  @media (min-width: 1900px) {
   }
 `;
 
@@ -80,28 +63,6 @@ export const BoxLeft = styled.div`
 
   @media (min-width: 1440px) {
     width: 30%;
-    height: 100%;
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    padding: 2rem 1rem;
-
-    & > img {
-      height: 100%;
-
-      transition: 0.5s;
-
-      :hover {
-        transform: rotate(-10deg) scale(0.9);
-
-        transition: 1.5s;
-      }
-    }
-  }
-
-  @media (min-width: 1900px) {
   }
 `;
 
@@ -139,26 +100,9 @@ export const BoxRigth = styled.div`
 
   @media (min-width: 1440px) {
     width: 50%;
-    height: 100%;
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
 
     & > p {
-      width: 80%;
-
       font-size: 25pt;
-      font-weight: bold;
-
-      text-align: center;
-
-      color: ${(props) => props.theme.textPrimary};
-
-      text-shadow: -5px -5px 0px ${(props) => props.theme.backgroundSecondary},
-        -5px 5px 0px ${(props) => props.theme.backgroundSecondary},
-        5px -5px 0px ${(props) => props.theme.backgroundSecondary},
-        5px 0px 0px ${(props) => props.theme.backgroundSecondary};
     }
   }
 
