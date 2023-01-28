2207bbf7c5718dcb95016b8fc500968083733441
Theus <matheusth.dev@gmail.com>
2023-01-28 20:07:48 -0300

    feat: bloco de rodapé 320

--- src/Blocks/FooterBlock/styles.ts
+++ src/Blocks/FooterBlock/styles.ts
@@ -2,6 +2,17 @@ import styled from "styled-components";
 
 export const Container = styled.section`
   @media (min-width: 320px) {
+    width: 100vw;
+    min-height: 450px;
+
+    display: flex;
+    flex-direction: column-reverse;
+    justify-content: center;
+    align-items: center;
+
+    padding: 0 10%;
+
+    background-color: ${(props) => props.theme.backgroundTertiary};
   }
 
   @media (min-width: 767px) {
@@ -25,6 +36,22 @@ export const Container = styled.section`
 
 export const BoxSocial = styled.div`
   @media (min-width: 320px) {
+    width: 90%;
+    height: 7rem;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+
+    & > h2 {
+      line-height: 2.1rem;
+
+      font-size: 18pt;
+
+      color: ${(props) => props.theme.tertiary};
+    }
   }
 
   @media (min-width: 767px) {
@@ -73,6 +100,27 @@ export const BoxSocial = styled.div`
 
 export const BoxIcons = styled.div`
   @media (min-width: 320px) {
+    width: 100%;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+
+    & > a > svg {
+      width: 30px;
+      height: 30px;
+
+      cursor: pointer;
+
+      transition: 0.3s;
+
+      :hover {
+        transform: scale(1.2) rotate(15deg);
+
+        transition: 1.5s;
+      }
+    }
   }
 
   @media (min-width: 767px) {
@@ -116,6 +164,23 @@ export const BoxIcons = styled.div`
 
 export const BoxLinks = styled.div`
   @media (min-width: 320px) {
+    width: 100%;
+    height: 10rem;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 0.5rem;
+
+    border-bottom: 1px solid ${(props) => props.theme.textTertiary};
+
+    & > h2 {
+      font-size: 16pt;
+      font-weight: bold;
+
+      color: ${(props) => props.theme.primary};
+    }
   }
 
   @media (min-width: 767px) {
@@ -131,6 +196,7 @@ export const BoxLinks = styled.div`
     padding-left: 1rem;
 
     border-right: 1px solid ${(props) => props.theme.textTertiary};
+    border-bottom: none;
 
     & > h2 {
       font-size: 16pt;
@@ -157,6 +223,38 @@ export const BoxLinks = styled.div`
 
 export const Links = styled.ul`
   @media (min-width: 320px) {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 0.5rem;
+
+    & > li {
+      list-style: none;
+
+      font-size: 12pt;
+      font-weight: bold;
+
+      color: ${(props) => props.theme.textTertiary};
+
+      cursor: pointer;
+
+      transition: 0.5s;
+
+      & > a {
+        text-decoration: none;
+
+        color: ${(props) => props.theme.textTertiary};
+      }
+
+      :hover {
+        transform: scale(0.9);
+
+        color: ${(props) => props.theme.tertiary};
+
+        transition: 1s;
+      }
+    }
   }
 
   @media (min-width: 767px) {
@@ -213,6 +311,48 @@ export const Links = styled.ul`
 
 export const BoxButton = styled.div`
   @media (min-width: 320px) {
+    width: 100%;
+    height: 0rem;
+
+    display: flex;
+    justify-content: flex-end;
+    align-items: flex-start;
+
+    & > a > div {
+      width: 3rem;
+      height: 3rem;
+
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      position: relative;
+      top: -35px;
+
+      border-radius: 50%;
+
+      box-shadow: 0 0 5px 0 #353535;
+
+      color: ${(props) => props.theme.white};
+      background-color: ${(props) => props.theme.secondary};
+
+      cursor: pointer;
+
+      transition: 0.5s;
+
+      & > svg {
+        width: 2.3rem;
+        height: 2.3rem;
+      }
+
+      :hover {
+        transform: scale(1.2) rotate(360deg);
+
+        box-shadow: 0 0 8px 0 #353535;
+
+        transition: 0.5s;
+      }
+    }
   }
 
   @media (min-width: 767px) {
