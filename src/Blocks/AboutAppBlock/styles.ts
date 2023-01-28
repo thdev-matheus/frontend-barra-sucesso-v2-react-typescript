2f8b61be753bbfb5ea942f1d80b27f898c65fea6
Theus <matheusth.dev@gmail.com>
2023-01-28 19:01:26 -0300

    feat: bloco sobre o app 320

--- src/Blocks/AboutAppBlock/styles.ts
+++ src/Blocks/AboutAppBlock/styles.ts
@@ -2,10 +2,21 @@ import styled from "styled-components";
 
 export const Container = styled.section`
   @media (min-width: 320px) {
+    width: 100vw;
+    min-height: 400px;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+
+    scroll-margin-top: 5rem;
+
+    background-color: ${(props) => props.theme.backgroundPrimary};
   }
 
   @media (min-width: 767px) {
-    width: 100vw;
     min-height: 350px;
 
     display: flex;
@@ -26,6 +37,29 @@ export const Container = styled.section`
 
 export const BoxTitle = styled.div`
   @media (min-width: 320px) {
+    width: 90%;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 0.3rem;
+
+    text-align: center;
+
+    & > span {
+      font-size: 9pt;
+      font-weight: bold;
+
+      color: ${(props) => props.theme.primary};
+    }
+
+    & > h2 {
+      font-size: 16pt;
+      font-weight: bold;
+
+      color: ${(props) => props.theme.textPrimary};
+    }
   }
 
   @media (min-width: 767px) {
@@ -73,6 +107,33 @@ export const BoxTitle = styled.div`
 
 export const BoxBody = styled.div`
   @media (min-width: 320px) {
+    width: 90%;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+
+    & > div {
+      width: 100%;
+
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      gap: 0.5rem;
+
+      text-align: center;
+
+      & > p {
+        width: 90%;
+
+        font-size: 10pt;
+
+        color: ${(props) => props.theme.textPrimary};
+      }
+    }
   }
 
   @media (min-width: 767px) {
@@ -128,6 +189,15 @@ export const BoxBody = styled.div`
 
 export const BoxButton = styled.div`
   @media (min-width: 320px) {
+    width: 100%;
+
+    display: flex;
+    justify-content: center;
+
+    & > div {
+      width: 10rem;
+      height: 3rem;
+    }
   }
 
   @media (min-width: 767px) {
