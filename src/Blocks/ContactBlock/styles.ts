5dac9aae2addddbd4f12ca668a8e2678834d9142
Theus <matheusth.dev@gmail.com>
2023-01-28 19:21:58 -0300

    feat: bloco de contato 320

--- src/Blocks/ContactBlock/styles.ts
+++ src/Blocks/ContactBlock/styles.ts
@@ -21,21 +21,7 @@ export const Container = styled.section<T.IContainerProps>`
   }
 
   @media (min-width: 767px) {
-    width: 100vw;
     min-height: 12.5rem;
-
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-
-    scroll-margin-top: 5rem;
-
-    background-color: ${(props) => props.theme.backgroundSecondary};
-    background-image: url(${(props) => props.bgImg});
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
   }
 
   @media (min-width: 1440px) {
@@ -110,26 +96,9 @@ export const BoxRigth = styled.div`
 
   @media (min-width: 767px) {
     width: 60%;
-    height: 100%;
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
 
     & > p {
-      width: 80%;
-
       font-size: 18pt;
-      font-weight: bold;
-
-      text-align: center;
-
-      color: ${(props) => props.theme.textPrimary};
-
-      text-shadow: -5px -5px 0px ${(props) => props.theme.backgroundSecondary},
-        -5px 5px 0px ${(props) => props.theme.backgroundSecondary},
-        5px -5px 0px ${(props) => props.theme.backgroundSecondary},
-        5px 0px 0px ${(props) => props.theme.backgroundSecondary};
     }
   }
 
