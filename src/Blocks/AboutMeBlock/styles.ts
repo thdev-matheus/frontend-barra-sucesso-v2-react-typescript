f9b60b5d2c9826dac577527badb7ce0e9eea0fca
Theus <matheusth.dev@gmail.com>
2023-01-27 23:36:26 -0300

    feat: bloco sobre mim 320

--- src/Blocks/AboutMeBlock/styles.ts
+++ src/Blocks/AboutMeBlock/styles.ts
@@ -16,17 +16,7 @@ export const Container = styled.section`
   }
 
   @media (min-width: 767px) {
-    width: 100vw;
     min-height: 28rem;
-
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-
-    scroll-margin-top: 5rem;
-
-    background-color: ${(props) => props.theme.backgroundSecondary};
   }
 
   @media (min-width: 1024px) {
@@ -106,29 +96,6 @@ export const BoxRight = styled.div`
     }
   }
 
-  @media (min-width: 767px) {
-    width: 80%;
-    height: 100%;
-
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: flex-end;
-    gap: 2rem;
-
-    & > section {
-      width: 100%;
-
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: flex-end;
-      gap: 1rem;
-
-      padding-right: 1rem;
-    }
-  }
-
   @media (min-width: 1024px) {
     width: 40%;
 
@@ -173,28 +140,12 @@ export const BoxTitle = styled.div`
   }
 
   @media (min-width: 767px) {
-    width: 100%;
-
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    gap: 0.3rem;
-
-    text-align: center;
-
     & > span {
       font-size: 11pt;
-      font-weight: bold;
-
-      color: ${(props) => props.theme.secondary};
     }
 
     & > h2 {
       font-size: 18pt;
-      font-weight: bold;
-
-      color: ${(props) => props.theme.textPrimary};
     }
   }
 
@@ -236,20 +187,8 @@ export const BoxBody = styled.div`
   }
 
   @media (min-width: 767px) {
-    width: 100%;
-
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    gap: 0.5rem;
-
-    text-align: center;
-
     & > p {
       font-size: 12pt;
-
-      color: ${(props) => props.theme.textPrimary};
     }
   }
 
@@ -263,18 +202,6 @@ export const BoxBody = styled.div`
 
 export const BoxButton = styled.div`
   @media (min-width: 320px) {
-    width: 100%;
-
-    display: flex;
-    justify-content: center;
-
-    & > div {
-      width: 10rem;
-      height: 3rem;
-    }
-  }
-
-  @media (min-width: 767px) {
     width: 100%;
 
     display: flex;
