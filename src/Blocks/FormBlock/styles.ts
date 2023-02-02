c5dfb062ae7060d3aa9fb0d0a723dfbe9ccd6e4c
Theus <matheusth.dev@gmail.com>
2023-02-01 23:14:33 -0300

    feat: estilização do editor de código e formulário 767

--- src/Blocks/FormBlock/styles.ts
+++ src/Blocks/FormBlock/styles.ts
@@ -5,6 +5,31 @@ export const Container = styled.form`
   }
 
   @media (min-width: 767px) {
+    width: 100vw;
+    height: calc(100vh - 5rem);
+
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 1.5rem;
+
+    padding: 1rem;
+
+    overflow: hidden auto;
+
+    background-color: ${(props) => props.theme.backgroundPrimamry};
+
+    & > p {
+      width: 60%;
+
+      font-size: 14pt;
+      font-weight: bold;
+
+      text-align: center;
+
+      color: ${(props) => props.theme.textPrimary};
+    }
   }
 
   @media (min-width: 1024px) {
@@ -47,6 +72,20 @@ export const BoxInput = styled.div`
   }
 
   @media (min-width: 767px) {
+    width: 60%;
+
+    & > .editor {
+      min-height: 14rem;
+
+      border-radius: 0.8rem;
+
+      font-weight: bold;
+
+      box-shadow: 2px 2px 5px 0 ${(props) => props.theme.gray};
+
+      background-color: ${(props) => props.theme.backgroundTertiary};
+      color: ${(props) => props.theme.white};
+    }
   }
 
   @media (min-width: 1024px) {
@@ -55,7 +94,14 @@ export const BoxInput = styled.div`
     & > .editor {
       min-height: 14rem;
 
-      border: 1px solid black;
+      border-radius: 0.8rem;
+
+      font-weight: bold;
+
+      box-shadow: 2px 2px 5px 0 ${(props) => props.theme.gray};
+
+      background-color: ${(props) => props.theme.backgroundTertiary};
+      color: ${(props) => props.theme.white};
     }
   }
 
@@ -71,6 +117,32 @@ export const BoxControls = styled.div`
   }
 
   @media (min-width: 767px) {
+    width: 60%;
+
+    display: flex;
+    justify-content: space-between;
+
+    padding: 0 2rem;
+    margin-top: 1rem;
+
+    & > h4 {
+      font-size: 16pt;
+      font-weight: bold;
+
+      color: ${(props) => props.theme.textPrimary};
+
+      cursor: pointer;
+
+      transition: 0.8s;
+
+      :hover {
+        transform: scale(1.1);
+
+        color: ${(props) => props.theme.primary};
+
+        transition: 0.8s;
+      }
+    }
   }
 
   @media (min-width: 1024px) {
@@ -114,6 +186,10 @@ export const BoxSelect = styled.div`
   }
 
   @media (min-width: 767px) {
+    width: 100%;
+
+    display: flex;
+    justify-content: center;
   }
 
   @media (min-width: 1024px) {
@@ -135,6 +211,12 @@ export const BoxButton = styled.div`
   }
 
   @media (min-width: 767px) {
+    width: 60%;
+    min-height: 3rem;
+
+    & > button {
+      border-radius: 0.5rem;
+    }
   }
 
   @media (min-width: 1024px) {
