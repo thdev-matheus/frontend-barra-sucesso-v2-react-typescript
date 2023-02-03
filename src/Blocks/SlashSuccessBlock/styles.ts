b1b2b2bdbd240e47416483c6e674a038cb50ae19
Theus <matheusth.dev@gmail.com>
2023-02-03 12:55:30 -0300

    feat: pagina do /sucesso 320

--- src/Blocks/SlashSuccessBlock/styles.ts
+++ src/Blocks/SlashSuccessBlock/styles.ts
@@ -2,6 +2,15 @@ import styled from "styled-components";
 
 export const Container = styled.section`
   @media (min-width: 320px) {
+    width: 100vw;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 1rem;
+
+    padding: 0.5rem 2% 2rem;
   }
 
   @media (min-width: 767px) {
@@ -25,6 +34,47 @@ export const Container = styled.section`
 
 export const BoxDescriptions = styled.div`
   @media (min-width: 320px) {
+    width: 90%;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 0.5rem;
+
+    text-align: center;
+
+    margin-top: 0.5rem;
+
+    & > h2 {
+      margin-bottom: 1rem;
+
+      font-size: 25pt;
+      font-weight: bold;
+
+      color: ${(props) => props.theme.textPrimary};
+    }
+
+    & > p {
+      font-size: 10pt;
+      font-weight: bold;
+
+      color: ${(props) => props.theme.textPrimary};
+
+      & > span {
+        color: ${(props) => props.theme.secondary};
+
+        cursor: pointer;
+
+        transition: 0.5s;
+
+        :hover {
+          color: ${(props) => props.theme.tertiary};
+
+          transition: 1.5s;
+        }
+      }
+    }
   }
 
   @media (min-width: 767px) {
@@ -80,6 +130,29 @@ export const BoxDescriptions = styled.div`
 
 export const BoxTextCopy = styled.div`
   @media (min-width: 320px) {
+    width: 90%;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    & > .editor {
+      width: 100%;
+      /* min-height: 14rem; */
+
+      margin-bottom: 1rem;
+
+      border-radius: 0.8rem;
+
+      font-weight: bold;
+
+      box-shadow: 2px 2px 5px 0 ${(props) => props.theme.gray};
+
+      background-color: ${(props) => props.theme.backgroundTertiary};
+      color: ${(props) => props.theme.white};
+      transform: translateY(20px);
+    }
   }
 
   @media (min-width: 767px) {
@@ -115,6 +188,40 @@ export const BoxTextCopy = styled.div`
 
 export const BoxCopy = styled.div`
   @media (min-width: 320px) {
+    width: 100%;
+    height: 0;
+
+    display: flex;
+    justify-content: flex-end;
+
+    & > svg {
+      width: 35px;
+      height: 35px;
+
+      position: relative;
+      right: 15px;
+      bottom: -35px;
+      z-index: 8999;
+
+      color: ${(props) => props.theme.tertiary};
+
+      cursor: pointer;
+
+      transition: 0.5s;
+
+      animation-name: shake;
+      animation-duration: 3s;
+      animation-fill-mode: forwards;
+      animation-iteration-count: infinite;
+
+      :hover {
+        animation-play-state: paused;
+
+        transition: 1.5s;
+
+        color: ${(props) => props.theme.primary};
+      }
+    }
   }
 
   @media (min-width: 767px) {
